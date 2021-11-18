d151dfda178b3c6eca7e20b2b9cc627962b4a894
monirwd <monirwd007@gmail.com>
2021-11-19 04:52:30 +0600

    header-area

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,36 @@
-(function($){
-	$(document).ready(function() {	
+(function ($) {
+	$(document).ready(function () {
 
 		// Scroll to Top
-		jQuery('.scrolltotop').click(function(){
-			jQuery('html').animate({'scrollTop' : '0px'}, 400);
+		jQuery('.scrolltotop').click(function () {
+			jQuery('html').animate({ 'scrollTop': '0px' }, 400);
 			return false;
 		});
-		
-		jQuery(window).scroll(function(){
+
+		jQuery(window).scroll(function () {
 			var upto = jQuery(window).scrollTop();
-			if(upto > 500) {
+			if (upto > 500) {
 				jQuery('.scrolltotop').fadeIn();
 			} else {
 				jQuery('.scrolltotop').fadeOut();
 			}
 		});
 
+		jQuery(".offcanvas-menu ul li a").click(function () {
+			jQuery(this).next().slideToggle()
+		})
 
-		
 
-				
-		
-		
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
+
+
+
+
+
 	});
 })(jQuery);
